8790d193e8f8df88888cc48a48a27957ff594b79
Chaitanya <chaitu.vsh@gmail.com>
2025-01-05 13:15:04 -0800

    Server component vs Client components

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+
+const Hello = () => {
+    console.log("I am a Client component.");
+  return (
+    <div>Hello :: Rendered from client component.</div>
+  )
+}
+
+export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Hello from "./components/hello";
+
 const page = () => {
   console.log("What am I ? -- SERVER/CLIENT ?");
   return (
     <>
       <h1 className="text-3xl">Welcome to Next.js</h1>
       <h3 className="text-1xl">This is the very first beginning of the crash course.</h3>
+      <Hello />
     </>
   )
 }
